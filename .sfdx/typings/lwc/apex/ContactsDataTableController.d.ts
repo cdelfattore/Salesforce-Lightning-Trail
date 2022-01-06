f61d172a4d145631a1092f893105eafcb8fae42d
Chris <cdelfattore@live.com>
2022-01-06 15:51:26 -0500

    ContactsSelector and contactsDataTable component

---
+++ .sfdx/typings/lwc/apex/ContactsDataTableController.d.ts
@@ -0,0 +1,3 @@
+declare module "@salesforce/apex/ContactsDataTableController.getContacts" {
+  export default function getContacts(param: {accountId: any}): Promise<any>;
+}
--- force-app/main/default/lwc/contactsDataTable/contactsDataTable.js
+++ force-app/main/default/lwc/contactsDataTable/contactsDataTable.js
@@ -1,6 +1,59 @@
 import { api, LightningElement } from 'lwc';
+import getContacts from '@salesforce/apex/ContactsDataTableController.getContacts';
+
+const actions = [
+    { label: 'Edit', name: 'edit' }
+];
+
+const columns = [
+    { label: 'Name', fieldName: 'name'},
+    { label: 'Phone', fieldName: 'phone'},
+    { 
+        type: 'action',
+        typeAttribute: {
+            rowActions : actions
+        }
+    }
+];
 
 export default class ContactsDataTable extends LightningElement
 {
     @api recordId;
+    isLoading = false;
+    columns = columns;
+    contacts = [];
+
+    connectedCallback()
+    {
+        this.loadContactsTable();
+    }
+
+    /**
+     * Call an apex method imperitavely 
+     */
+    loadContactsTable()
+    {
+        this.loading();
+        getContacts({accountId : this.recordId})
+            .then(result => {
+                this.contacts = result.contacts;
+            })
+            .catch(error => {
+                debugger;
+            })
+            .finally(() => {
+                this.notLoading();
+            })
+    }
+
+    loading()
+    {
+        this.isLoading = true;
+    }
+
+    notLoading()
+    {
+        this.isLoading = false;
+    }
+
 }
